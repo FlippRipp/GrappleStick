9519d7d21d8d3eecbfd6099534d9b349a609b220
Filip <filip@sigrand.se>
2021-11-11 16:08:41 +0100

    a lot of gravity stuff

---
+++ Assets/Scripts/Enviroment/LayerSwitcher.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private float switchTime = 5;
+
+    private int grappleLayer;
+    private int groundLayer;
+    
+}
