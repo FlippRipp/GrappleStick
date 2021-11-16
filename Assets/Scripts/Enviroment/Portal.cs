8b28e456cc7f4b9ed7ca08f2802c683e20bf48ca
Filip <filip@sigrand.se>
2021-11-16 16:19:37 +0100

    portalllllllllllllllllllllllllllllllllllllllllllllllllll

---
+++ Assets/Scripts/Enviroment/Portal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [HideInInspector]
+    public PortalController portalController;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        if (rb)
+        {
+            Debug.Log("aaaaaa");
+            portalController.Teleport(this, rb);
+        }
+    }
+}
